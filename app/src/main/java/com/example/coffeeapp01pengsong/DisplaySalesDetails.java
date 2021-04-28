fc4b4d72b9a9d70636ecdbd1b3890cf278a80143
xdd <2235243406@qq.com>
2021-04-28 19:22:35 +0800

    Testing

---
+++ app/src/main/java/com/example/coffeeapp01pengsong/DisplaySalesDetails.java
@@ -0,0 +1,22 @@
+package com.example.coffeeapp01pengsong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplaySalesDetails extends AppCompatActivity {
+    String message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_sales_details);
+
+        Intent intent = getIntent();
+        message = intent.getStringExtra("db");
+        TextView salesView = (TextView)findViewById(R.id.displayTextView);
+        salesView.setText(message);
+    }
+}
